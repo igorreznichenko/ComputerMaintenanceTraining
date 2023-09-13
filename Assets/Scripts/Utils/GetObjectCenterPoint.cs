e4ba60b836a4b3bb8edab5cd9ed740dadd217123
Ihor Reznichenko <igorteamhtc@live.ru>
2023-09-13 10:38:05 +0300

    feat(Motherboard): extract materials and textures
    feat(Lobby): add PC case with motherboard

---
+++ Assets/Scripts/Utils/GetObjectCenterPoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComputerMaintenanceTraining.Utils
+{
+    public class GetObjectCenterPoint : MonoBehaviour
+    {
+
+    }
+}
