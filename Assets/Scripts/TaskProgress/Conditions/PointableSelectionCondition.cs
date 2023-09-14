7850c9f5d84856daafe39ad32ace3b1580f6219f
Ihor Reznichenko <igorteamhtc@live.ru>
2023-09-14 17:39:36 +0300

    feat(BatteryHolder): create logic, create prefab, add conditions
    feat(Battery): add place to placeholder condition

---
+++ Assets/Scripts/TaskProgress/Conditions/PointableSelectionCondition.cs
@@ -0,0 +1,44 @@
+using Oculus.Interaction;
+using System;
+using UnityEngine;
+
+namespace ComputerMaintenanceTraining.TaskProgress.Conditions
+{
+	public class PointableSelectionCondition : Condition
+	{
+		[SerializeField]
+		private PointableUnityEventWrapper _pointableUnityEventWrapper;
+
+		private void OnEnable()
+		{
+			SubscribeEvents();
+		}
+
+		private void OnDisable()
+		{
+			UnsubscribeEvents();
+		}
+
+		private void UnsubscribeEvents()
+		{
+			_pointableUnityEventWrapper.WhenSelect.AddListener(OnSelectEventHandler);
+			_pointableUnityEventWrapper.WhenUnselect.AddListener(OnUnselectEventHandler);
+		}
+
+		private void SubscribeEvents()
+		{
+			_pointableUnityEventWrapper.WhenSelect.RemoveListener(OnSelectEventHandler);
+			_pointableUnityEventWrapper.WhenUnselect.RemoveListener(OnUnselectEventHandler);
+		}
+
+		private void OnSelectEventHandler(PointerEvent args)
+		{
+			Satisfied = true;
+		}
+
+		private void OnUnselectEventHandler(PointerEvent args)
+		{
+			Satisfied = false;
+		}
+	}
+}
