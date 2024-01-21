13e8a27dbecc4a21e1431235e33d8b2f1b24b528
Ihor Reznichenko <igorteamhtc@live.ru>
2024-01-22 00:09:40 +0200

    feat(FinishScreen): add auto restart experience logic

--- Assets/Scripts/UI/Monitor/FinishScreen.cs
+++ Assets/Scripts/UI/Monitor/FinishScreen.cs
@@ -1,4 +1,7 @@
+using ComputerMaintenanceTraining.Constants;
 using ComputerMaintenanceTraining.Extensions;
+using ComputerMaintenanceTraining.SceneManagement;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -10,11 +13,22 @@ namespace ComputerMaintenanceTraining.UI.Monitor
 		private TMP_Text _finishText;
 
 		[SerializeField]
+		private TMP_Text _restartInfo;
+
+		[SerializeField]
+		private SceneSwitcher _sceneLoader;
+
+		[SerializeField]
+		private int _secondsToRestart;
+
+		[SerializeField]
 		private float _printTextSpeed;
 
 		private Coroutine _printTextCoroutine;
 
 		private const string _text = "Finish";
+
+		private const string _restartTextTemplate = "Restart in {0} seconds";
 
 		private void OnEnable()
 		{
@@ -24,7 +38,26 @@ namespace ComputerMaintenanceTraining.UI.Monitor
 		private void PrintFinishText()
 		{
 			this.KillCoroutine(ref _printTextCoroutine);
-			_printTextCoroutine = StartCoroutine(_finishText.PrintTextCoroutine(_text, _printTextSpeed));
+			_printTextCoroutine = StartCoroutine(PrintFinishTextCoroutine());
+		}
+
+		private IEnumerator PrintFinishTextCoroutine()
+		{
+			int currentTimeToRestart = _secondsToRestart;
+
+			yield return _finishText.PrintTextCoroutine(_text, _printTextSpeed);
+
+			yield return _restartInfo.PrintTextCoroutine(string.Format(_restartTextTemplate, currentTimeToRestart), _printTextSpeed);
+
+			while (currentTimeToRestart > 0)
+			{
+				yield return new WaitForSeconds(1);
+
+				currentTimeToRestart--;
+				_restartInfo.text = string.Format(_restartTextTemplate, currentTimeToRestart);
+			}
+
+			_sceneLoader.SwitchSceneAfterFade(SceneNames.ASSEMBLY_SCENE_NAME);
 		}
 	}
 }
