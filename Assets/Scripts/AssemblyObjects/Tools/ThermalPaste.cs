ba03f8e9b4a51e23817161e82d700fa7897a720c
Ihor Reznichenko <igorteamhtc@live.ru>
2024-01-21 23:27:00 +0200

    refactor(ThermalPaste): remove unusable code lines

--- Assets/Scripts/AssemblyObjects/Tools/ThermalPaste.cs
+++ Assets/Scripts/AssemblyObjects/Tools/ThermalPaste.cs
@@ -10,23 +10,12 @@ namespace ComputerMaintenanceTraining.AssemblyObjects
 	public class ThermalPaste : AssemblyObject, ITriggerEnterHandler
 	{
 		[SerializeField]
-		private PointableUnityEventWrapper _pointableUnityEventWrapper;
-
-		[SerializeField]
 		private Transform _syringeHandle;
 
 		[SerializeField]
 		private Transform _syringeHandlePushIn;
 
 		private bool _hasThermalPaste = true;
-
-		public event Action<IPlaceholderObject> OnPlacableStateChanged;
-		public event Action<PlaceholderPlace> OnPlaceholderPlaceChanged;
-
-		public void OnPlaceholderPlaceChangedEventHandler(PlaceholderPlace placeholderPlace)
-		{
-			OnPlaceholderPlaceChanged?.Invoke(placeholderPlace);
-		}
 
 		public void TriggerEnter(Collider other)
 		{
